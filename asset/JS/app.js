cf4e10c6dcf33ee2e969dd744e6587cbf8fe8e50
ElshanAli <elshanml@code.edu.az>
2022-07-04 17:18:15 +0400

    Added task about Confitm Method

---
+++ asset/JS/app.js
@@ -0,0 +1,9 @@
+function Clickbutton() {
+    if (confirm('if you cancel you won`t be able to see anything')==false) {
+        document.body.style.display='none'
+    }else{
+        document.body.style.display='visible'
+        
+    }
+}
+    
